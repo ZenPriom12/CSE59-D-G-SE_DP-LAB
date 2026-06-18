202f920edc6b2086161162ee97ac49ac35c28ca9
Priom Dhar <priomdhar10001@gmail.com>
2026-06-18 16:31:24 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,21 @@
+class Sensor {
+    String name;
+    int batteryLevel;
+
+    void charge()
+    {
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args)
+    {
+        Sensor sensor = new Sensor();
+
+        sensor.charge();
+        System.out.println(sensor.batteryLevel);
+    }
+}
 
